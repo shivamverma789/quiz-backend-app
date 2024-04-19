d99c198f3f9c39f392da789d9e5a1b33bae33deb
shivamverma789 <shivamjitendraverma@gmail.com>
2024-04-19 20:11:52 +0530

    change rate limit

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -2,8 +2,8 @@ const rateLimit = require('express-rate-limit');
 
 // Create a rate limiter with the desired options
 const limiter = rateLimit({
-    windowMs: 1 * 60 * 1000, // 15 minutes
-    max: 1, // Limit each IP to 100 requests per windowMs
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
     message: 'Too many requests from this IP, please try again later'
 });
 
